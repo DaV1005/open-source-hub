5bc3798b06d55bd9334cf81af1a4646ae069a498
DaV1005 <davidviers100598@gmail.com>
2024-11-16 15:46:32 -0800

    updated

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 || 3001;
 
 app.use(cors({
   origin: 'http://localhost:5173', 
