9bb4860bcd3af019d78ab148d3702266de5bac84
DaV1005 <davidviers100598@gmail.com>
2024-11-16 22:32:59 -0800

    fixed cor

--- server/server.js
+++ server/server.js
@@ -7,10 +7,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000 || 3001;
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true, 
-}));
+app.use(cors());
 app.use(express.json());
 
 
